956ab3fb03be1cc2ff6c22370e8ad0805f68da3e
Levi Zitting <levizitting5663@gmail.com>
2017-06-05 14:38:06 -0500

    Adding files for ep 8

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,7 @@
 |
 */
 
-Route::get('/tasks', function () {
-    $tasks = DB::table('tasks')->get();
 
-    return view('tasks/index', compact('tasks'));
-});
+Route::get('/tasks', 'TasksController@index');
 
-Route::get('/tasks/{task}', function($id) {
-
-    $task = DB::table('tasks')->find($id);
-
-    return view('tasks/show', compact('task'));
-});
+Route::get('/tasks/{task}', 'TasksController@show');
