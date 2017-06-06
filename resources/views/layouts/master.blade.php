158018af2caf0eb90946948192658bc6be3d3c3c
Levi Zitting <levizitting5663@gmail.com>
2017-06-05 19:24:42 -0500

    Adding files for ep 9-10

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Album example for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="/css/album.css" rel="stylesheet">
+</head>
+
+<body>
+
+@include('layouts/nav')
+
+@yield('content')
+
+@include('layouts/footer')
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+</body>
+</html>
