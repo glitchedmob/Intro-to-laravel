43dbb28e4b4ec4997497dfcc4b40300af08024f8
Levi Zitting <levizitting5663@gmail.com>
2017-06-07 15:39:46 -0500

    updating to ep 28

---
+++ app/Http/Requests/RegistrationForm.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\User;
+use App\Mail\Welcome;
+use Illuminate\Support\Facades\Mail;
+
+class RegistrationForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed'
+        ];
+    }
+
+    public function persist()
+    {
+        $user = User::create([
+            'name' => $this->only('name'),
+            'email' => $this->only('email'),
+            'password' => bcrypt($this->only('password')[0])
+        ]);
+
+        auth()->login($user);
+
+        Mail::to($user)->send(new Welcome($user));
+    }
+}
