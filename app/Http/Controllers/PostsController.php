df415ddeedd5b8175a7a827729ae7ac6a60ce52e
Levi Zitting <levizitting5663@gmail.com>
2017-06-07 00:27:41 -0500

    Adding files for up to ep 20

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,13 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Carbon\Carbon;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     public function index()
     {
-        $posts = Post::latest()->get();
-        return view("posts/index", compact('posts'));
+        $posts = Post::latest()
+            ->filter(request(['month', 'year']))
+            ->get();
+
+
+        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
+            ->groupBy('year', 'month')
+            ->orderByRaw ('min(created_at)')
+            ->get()
+            ->toArray();
+
+        return view("posts/index", compact('posts', 'archives'));
     }
 
     public function show(Post $post)
@@ -30,7 +46,9 @@ class PostsController extends Controller
             'body' => 'required'
         ]);
 
-        Post::create(request(['title', 'body']));
+        auth()->user()->publish(
+            new Post(request(['title', 'body']))
+        );
 
         return redirect('/');
     }
--- app/Post.php
+++ app/Post.php
@@ -3,18 +3,35 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'body'];
+    protected $fillable = ['title', 'body', 'user_id'];
 
     public function comments()
     {
         return $this->hasMany(Comment::class);
     }
 
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function addComment($body)
     {
         $this->comments()->create(compact('body'));
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        if($month = $filters['month']) {
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if($year = $filters['year']) {
+            $query->whereYear('created_at', $year);
+        }
     }
 }
